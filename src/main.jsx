80f858325e15b95b318797c74212b8a2a3b02a99
shinis(home) <actionshin@gmail.com>
2025-07-02 23:36:51 +0900

    Deleting Records

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,8 @@ import Contact, {
 import EditContact, {
   action as editAction,
 } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+
 
 const router = createBrowserRouter([
   {
@@ -35,6 +37,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ]
   },
