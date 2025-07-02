b874b7e4c365577a367fd2a73bed55e3c6f5b5fd
shinis(home) <actionshin@gmail.com>
2025-07-02 23:56:17 +0900

    Synchronizing URLs to Form State(2)

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -8,6 +8,7 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
 
 export async function action() {
   const contact = await createContact();
@@ -24,6 +25,11 @@ export async function loader({ request }) {
 export default function Root() {
   const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
+  
   return (
     <>
       <div id="sidebar">
